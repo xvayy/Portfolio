0bf8b53f690e6a54522d3d08ae57b48ca56b5066
Yula <ivanykjula@gmail.com>
2023-05-07 00:41:35 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,114 @@
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active')
+            body.classList.remove('locked')
+        }
+    })
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', events => {
+    events.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substring(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
+
+burgerMenu()
+
+
+// Modall
+function bindModal(trigger, modal, close) {
+  trigger = document.querySelector(trigger),
+    modal = document.querySelector(modal),
+    close = document.querySelector(close)
+ 
+  const body = document.body
+
+  trigger.addEventListener('click', e => {
+    e.preventDefault()
+    modal.style.display = 'flex'
+    body.classList.add('locked')
+  });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none'
+    body.classList.remove('locked')
+  });
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      modal.style.display = 'none'
+      body.classList.remove('locked')
+    }
+  })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+
+
+//Tabs
+
+function tabs(headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector)
+  function hideTabContent() {
+    content.forEach(item => {
+      item.style.display = 'none'
+    });
+    tab.forEach(item => {
+      item.classList.remove(activeClass)
+    });
+  }
+  function showTabContent(i = 0) {
+    content[i].style.display = display
+    tab[i].classList.add(activeClass)
+  }
+  hideTabContent()
+  showTabContent()
+  header.addEventListener('click', e => {
+    const target = e.target
+    if (target.classList.contains(tabSelector.replace(/\./, '')) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent()
+          showTabContent(i)
+        }
+      });
+    }
+  })
+}
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
